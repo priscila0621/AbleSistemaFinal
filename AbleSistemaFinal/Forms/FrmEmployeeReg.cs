fdd004d99440f98eeb6a2bb2f47da9b985c0b357
gague-uam <ggpaiz@uamv.edu.ni>
2024-11-30 21:59:56 -0600

    Se arregla validacion de datos. Se agrego la hora dentro de los reportes.

--- AbleSistemaFinal/Forms/FrmEmployeeReg.cs
+++ AbleSistemaFinal/Forms/FrmEmployeeReg.cs
@@ -117,7 +117,7 @@ namespace AbleSistemaFinal.Forms
                 return false;
             }
 
-            if (TbIdNumber.Text.Length != 12)
+            if (TbIdNumber.Text.Length != 19)
             {
                 MessageBox.Show("Ingrese una cédula válida", "Error de Validación", MessageBoxButtons.OK, MessageBoxIcon.Error);
                 return false;
